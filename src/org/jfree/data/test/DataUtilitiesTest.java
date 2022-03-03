57fb3567258080b4574e3d69aa92622885410770
Peter Campbell <pcampbell52@qub.ac.uk>
2022-03-03 13:48:30 +0000

    Added Range and DataUtilities tests

---
+++ src/org/jfree/data/test/DataUtilitiesTest.java
@@ -0,0 +1,46 @@
+package org.jfree.data.test;
+
+import org.jfree.data.DataUtilities;
+import org.jfree.data.DefaultKeyedValues2D;
+import org.jfree.data.Values2D;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class DataUtilitiesTest extends TestCase {
+	
+	private Values2D values2D;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	protected void setUp() throws Exception {
+//		super.setUp();
+		DefaultKeyedValues2D testValues = new DefaultKeyedValues2D();
+		values2D = testValues;
+		testValues.addValue(1, 0, 0);
+		testValues.addValue(4, 1, 0);
+	}
+
+	@After
+	protected void tearDown() throws Exception {
+//		super.tearDown();
+		values2D = null;
+	}
+
+	@Test
+	public void testValidDataAndColumnColumnTotal() {
+		assertEquals("Wrong sum returned. It should be 5.0", 5.0, DataUtilities.calculateColumnTotal(values2D, 0), 0.000000001d);
+	}
+
+}
---
+++ src/org/jfree/data/test/RangeTest.java
@@ -0,0 +1,40 @@
+package org.jfree.data.test;
+
+import static org.junit.Assert.*;
+
+import org.jfree.data.Range;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class RangeTest extends TestCase {
+	
+	private Range rangeObjectUnderTest;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		rangeObjectUnderTest = new Range(-1, 1);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testCentralValueShouldBeZero() {
+		assertEquals("The central value of -1 and 1 should be 0", 0, rangeObjectUnderTest.getCentralValue(), 0.000000001d);
+	}
+
+}
