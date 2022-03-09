77e32cf0440975ed967fd90afbcf38eb8b1b0d8a
michaelshorten <83963174+michaelshorten@users.noreply.github.com>
2022-03-09 15:37:54 +0000

    Changed Methods

--- src/org/jfree/data/test/RangeTest.java
+++ src/org/jfree/data/test/RangeTest.java
@@ -40,21 +40,31 @@ public class RangeTest extends TestCase {
 	public void testCentralValueShouldBeZero() {
 		assertEquals("The central value of -1 and 1 should be 0", 0, rangeObjectUnderTest.getCentralValue(), 0.000000001d);
 	}
+	// CONTAINS TESTING METHODS
 	@Test
 	public void testContainsInRange() {
 		assertEquals("Range should contain 3 and return True", true, rangeContains.contains(3));		assertEquals("Range shouldn't contain 20 and return false", false, rangeContains.contains(20));
 	}
-	public void testContainsOutsideRange() {		assertEquals("Range should contain 3 and return True", true, rangeContains.contains(3));
+	@Test
+	public void testContainsAboveRange() {		
 		assertEquals("Range shouldn't contain 20 and return false", false, rangeContains.contains(20));
 	}
+	@Test
+	public void testContainsBelowRange() {
+	assertEquals("Range shouldn't contain -5 and return false", false, rangeContains.contains(-5));
+    }
+	// CONSTRAIN TESTING METHODS
+	@Test
 	public void testConstrainInRange() {
 		System.out.print("Expected: 3 Actual: " + rangeConstrain.constrain(3) + " ");
 		assertEquals("Range should contain 3 and return 3", 3, rangeConstrain.constrain(3),0.000000001d);	
 	}
+	@Test
 	public void testConstrainAboveRange() {	
 		System.out.print("Expected: 5 Actual: " + rangeConstrain.constrain(20)+" ");
 		assertEquals("Range shouldnt contain 20 but return clostest value in range with 5", 5, rangeConstrain.constrain(20),0.000000001d);
 	}
+	@Test
 	public void testConstrainBelowRange() {
 		System.out.print("Expected: 1 Actual: " + rangeConstrain.constrain(-20)+" ");
 		assertEquals("Range shouldnt contain -20 but return clostest value in range with 1", 5, rangeConstrain.constrain(-20),0.000000001d);
